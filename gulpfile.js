a69ec2479eacb1eaa19759f4daaa28979b57509c
Arthur Weber <darkw@PC-Weber>
2024-12-30 21:04:19 -0300

    Configurações iniciais

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'))
+}
+
+function compStyles() {
+    return gulp.src('./src/styles/*scss')
+        .pipe(sass({
+            outputStyle: 'compressed',
+            includePaths: ['./node_modules/bootstrap/scss']
+        }))
+        .pipe(gulp.dest('./dist/css/'));
+}
+
+function compImg() {
+    return gulp.src('./src/img/**/*', { encoding: false })
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+}
+
+exports.default = gulp.parallel(compStyles, compImg, scripts);
+exports.watch = function () {
+    gulp.watch('./src/styles/*scss', gulp.parallel(compStyles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
+}
